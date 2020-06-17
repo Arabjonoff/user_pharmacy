6d5548bd4ba7fc3c05340bf4eb4d4b66257638dd
Shahboz <shahboz.turonov98@gmail.com>
2020-06-17 15:28:02 +0500

    sub category

--- lib/ui/main/catalog/sub_catalog_screen.dart
+++ lib/ui/main/catalog/sub_catalog_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_translate/global.dart';
@@ -5,6 +7,7 @@ import 'package:page_transition/page_transition.dart';
 import 'package:pharmacy/model/category_model.dart';
 import 'package:pharmacy/ui/item_list/item_list_screen.dart';
 import 'package:pharmacy/ui/search/search_screen.dart';
+import 'package:pharmacy/utils/utils.dart';
 
 import '../../../app_theme.dart';
 
@@ -28,41 +31,37 @@ class _SubCategoryScreenState extends State<SubCategoryScreen> {
   Widget build(BuildContext context) {
     size = MediaQuery.of(context).size;
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: AppTheme.white,
+        brightness: Brightness.light,
+        leading: IconButton(
+          icon: Icon(
+            Platform.isIOS ? Icons.arrow_back_ios : Icons.arrow_back,
+            color: AppTheme.black_catalog,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text(
+          widget.name,
+          textAlign: TextAlign.start,
+          style: TextStyle(
+            //fontFamily: "Sofia",
+            fontSize: 21,
+            fontFamily: AppTheme.fontCommons,
+            fontWeight: FontWeight.normal,
+            color: AppTheme.black_text,
+          ),
+        ),
+      ),
+      backgroundColor: AppTheme.white,
       body: Stack(
         children: <Widget>[
           Container(
-            height: 104,
             width: size.width,
-            color: AppTheme.green_app_color,
-            child: Container(
-              margin: EdgeInsets.only(top: 24, left: 3, bottom: 24),
-              width: size.width,
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Row(
-                  children: <Widget>[
-                    IconButton(
-                      icon: Icon(
-                        Icons.arrow_back,
-                        color: Colors.white,
-                      ),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    ),
-                    Text(
-                      widget.name,
-                      style: TextStyle(color: Colors.white, fontSize: 21),
-                      maxLines: 1,
-                    )
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 104),
-            height: size.height - 104,
+            margin: EdgeInsets.only(top: 56),
             child: ListView.builder(
               itemCount: widget.list.length,
               itemBuilder: (context, position) {
@@ -84,6 +83,7 @@ class _SubCategoryScreenState extends State<SubCategoryScreen> {
                         color: AppTheme.white,
                         child: Container(
                           height: 48,
+                          padding: EdgeInsets.only(top: 6, bottom: 6),
                           margin: EdgeInsets.only(left: 15, right: 15),
                           child: Row(
                             children: <Widget>[
@@ -91,14 +91,16 @@ class _SubCategoryScreenState extends State<SubCategoryScreen> {
                                 child: Text(
                                   widget.list[position].name,
                                   style: TextStyle(
-                                      color: AppTheme.dark_grey,
-                                      fontWeight: FontWeight.w600),
+                                    fontSize: 17,
+                                    color: AppTheme.black_catalog,
+                                    fontFamily: AppTheme.fontSFProText,
+                                  ),
                                 ),
                               ),
                               Icon(
                                 Icons.arrow_forward_ios,
                                 size: 16,
-                                color: AppTheme.green_app_color,
+                                color: AppTheme.arrow_catalog,
                               )
                             ],
                           ),
@@ -115,52 +117,71 @@ class _SubCategoryScreenState extends State<SubCategoryScreen> {
             ),
           ),
           Container(
-            margin: EdgeInsets.only(top: 80, left: 15, right: 15, bottom: 15),
             height: 48,
-            width: double.infinity,
-            child: Material(
-              elevation: 5.0,
-              borderRadius: BorderRadius.circular(9.0),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    PageTransition(
-                      type: PageTransitionType.fade,
-                      child: SearchScreen(""),
-                    ),
-                  );
-                },
-                child: Row(
-                  children: <Widget>[
-                    IconButton(
-                      icon: new Icon(
-                        Icons.search,
-                        size: 24,
-                        color: AppTheme.green_app_color,
-                      ),
-                      onPressed: (){},
-                    ),
-                    Expanded(
-                      child: Text(
-                        translate("search_hint"),
-                      ),
-                    ),
-                    IconButton(
-                      icon: new Icon(
-                        Icons.scanner,
-                        size: 24,
-                        color: Colors.black45,
-                      ),
-                      onPressed: () {
-                        print("click");
-                      },
-                    ),
-                  ],
-                ),
-              ),
+            width: size.width,
+            padding: EdgeInsets.only(
+              top: 6,
+              bottom: 6,
             ),
-          )
+            margin: EdgeInsets.only(
+              left: 12,
+              right: 12,
+            ),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(9.0),
+                      color: AppTheme.black_transparent,
+                    ),
+                    child: Row(
+                      children: [
+                        IconButton(
+                          icon: new Icon(
+                            Icons.search,
+                            size: 24,
+                            color: AppTheme.notWhite,
+                          ),
+                          onPressed: () {},
+                        ),
+                        Expanded(
+                          child: Text(
+                            translate("search_hint"),
+                            style: TextStyle(
+                              color: AppTheme.notWhite,
+                              fontSize: 17,
+                              fontFamily: AppTheme.fontSFProText,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  child: Container(
+                    margin: EdgeInsets.only(left: 15),
+                    child: Center(
+                      child: Image.asset("assets/images/scanner.png"),
+                    ),
+                  ),
+                  onTap: () {
+                    var response = Utils.scanBarcodeNormal();
+                    response.then(
+                      (value) => Navigator.push(
+                        context,
+                        PageTransition(
+                          type: PageTransitionType.fade,
+                          child: SearchScreen(value),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
