4675aac5cc9d03b2f244fd0ba4d7ad4316ca337a
Shahboz <shahboz.turonov98@gmail.com>
2020-06-25 19:05:40 +0500

    menun

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:flutter_translate/localization_provider.dart';
 import 'package:flutter_translate/localized_app.dart';
 import 'package:pharmacy/ui/main_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shimmer/shimmer.dart';
 
 import 'app_theme.dart';
 
@@ -60,6 +61,78 @@ class MyApp extends StatelessWidget {
           platform: TargetPlatform.iOS,
         ),
         home: MainScreen(),
+      ),
+    );
+  }
+}
+
+class LoadingListPage extends StatefulWidget {
+  @override
+  _LoadingListPagState createState() => _LoadingListPagState();
+}
+
+class _LoadingListPagState extends State<LoadingListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Shimmer.fromColors(
+        baseColor: Colors.grey[300],
+        highlightColor: Colors.grey[100],
+        child: ListView.builder(
+          padding: const EdgeInsets.only(
+            right: 12,
+            left: 12,
+          ),
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (_, __) => Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: Container(
+              width: 140,
+              height: 250,
+              padding: EdgeInsets.only(
+                right: 12,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    width: 140.0,
+                    height: 140.0,
+                    color: Colors.white,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10.0),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: 15.0,
+                    color: Colors.white,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 3.0),
+                  ),
+                  Container(
+                    width: 40.0,
+                    height: 14.0,
+                    color: Colors.white,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 11.0),
+                  ),
+                  Container(
+                    height: 30,
+                    width: 120,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          itemCount: 6,
+        ),
       ),
     );
   }
