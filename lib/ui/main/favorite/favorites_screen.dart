bfa91171fe63896e3f0f406016534be21d7f22e4
Shahboz <shahboz.turonov98@gmail.com>
2020-06-27 10:11:59 +0500

    favourite

--- lib/ui/main/favorite/favorites_screen.dart
+++ lib/ui/main/favorite/favorites_screen.dart
@@ -28,80 +28,82 @@ class _FavoritesScreenState extends State<FavoritesScreen> {
 
     return Scaffold(
       backgroundColor: AppTheme.white,
-      body: Stack(
-        children: [
-          Container(
-            margin: EdgeInsets.only(top: 64),
-            child: FutureBuilder<List<ItemResult>>(
-              future: dataBase.getProdu(false),
-              builder: (context, snapshot) {
-                allCount = 0;
-                var data = snapshot.data;
-                if (data == null) {
-                  return Container(
-                    child: Center(
-                      child: Text("error"),
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(63.0),
+          child: AppBar(
+            automaticallyImplyLeading: false,
+            elevation: 1.0,
+            backgroundColor: AppTheme.white,
+            brightness: Brightness.light,
+            title: Container(
+              height: 63,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 3.5),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          translate("main.favourite"),
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                            color: AppTheme.black_text,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: AppTheme.fontCommons,
+                            fontSize: 17,
+                          ),
+                        ),
+                        Text(
+                          translate(
+                            count.toString() + " " + translate("item.tovar"),
+                          ),
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                            color: AppTheme.black_transparent_text,
+                            fontWeight: FontWeight.normal,
+                            fontFamily: AppTheme.fontRoboto,
+                            fontSize: 13,
+                          ),
+                        ),
+                      ],
                     ),
-                  );
-                }
-                count = data.length;
-                for (int i = 0; i < data.length; i++) {
-                  allCount += data[i].cardCount;
-                }
-                return data.length == 0
-                    ? FavoriteEmptyScreen()
-                    : ListView.builder(
-                        shrinkWrap: true,
-                        physics: ClampingScrollPhysics(),
-                        scrollDirection: Axis.vertical,
-                        itemCount: data.length,
-                        itemBuilder: (context, index) {
-                          return ItemView(data[index]);
-                        },
-                      );
-              },
+                  ),
+                ],
+              ),
             ),
-          ),
-          Container(
-            height: 1,
-            color: AppTheme.black_linear_category,
-            margin: EdgeInsets.only(top: 87),
-          ),
-          Container(
-            color: AppTheme.white,
-            height: 63,
-            width: size.width,
-            margin: EdgeInsets.only(top: 24),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  translate("main.favourite"),
-                  textAlign: TextAlign.start,
-                  style: TextStyle(
-                    color: AppTheme.black_text,
-                    fontWeight: FontWeight.w500,
-                    fontFamily: AppTheme.fontCommons,
-                    fontSize: 17,
-                  ),
-                ),
-                Text(
-                  translate(
-                    count.toString() + " " + translate("item.tovar"),
-                  ),
-                  textAlign: TextAlign.start,
-                  style: TextStyle(
-                    color: AppTheme.black_transparent_text,
-                    fontWeight: FontWeight.normal,
-                    fontFamily: AppTheme.fontRoboto,
-                    fontSize: 13,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+          )),
+      body: FutureBuilder<List<ItemResult>>(
+        future: dataBase.getProdu(false),
+        builder: (context, snapshot) {
+          allCount = 0;
+          var data = snapshot.data;
+          if (data == null) {
+            return Container(
+              child: Center(
+                child: Text("error"),
+              ),
+            );
+          }
+          count = data.length;
+          for (int i = 0; i < data.length; i++) {
+            allCount += data[i].cardCount;
+          }
+          return data.length == 0
+              ? FavoriteEmptyScreen()
+              : ListView.builder(
+            shrinkWrap: true,
+            physics: ClampingScrollPhysics(),
+            scrollDirection: Axis.vertical,
+            itemCount: data.length,
+            itemBuilder: (context, index) {
+              return ItemView(data[index]);
+            },
+          );
+        },
       ),
     );
   }
