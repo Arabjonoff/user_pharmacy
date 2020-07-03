542555a5afe013f4f6030308396718f407ed0fae
KOSSHOH <shahboz.turonov98@gmail.com>
2020-07-03 10:23:07 +0500

    card order

--- lib/src/ui/shopping/order_card.dart
+++ lib/src/ui/shopping/order_card.dart
@@ -1,7 +1,14 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:flutter_translate/global.dart';
+import 'package:pharmacy/src/blocs/card_bloc.dart';
+import 'package:pharmacy/src/database/database_helper.dart';
+import 'package:pharmacy/src/model/api/item_model.dart';
+import 'package:pharmacy/src/ui/main/card/card_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shimmer/shimmer.dart';
 
 import '../../app_theme.dart';
 
@@ -21,13 +28,18 @@ class CheckboxList {
 }
 
 class _OrderCardScreenState extends State<OrderCardScreen> {
-  String radioItemHolder = 'Наличные';
+  String radioItemHolder = '8600 45** **** 3103';
   int id = 1;
+  int allCount = 0;
+  double allPrice = 0;
+
+  String fullName = "";
+  String number = "";
 
   List<CheckboxList> nList = [
     CheckboxList(
       index: 1,
-      number: "Наличные",
+      number: "8600 45** **** 3103",
     ),
     CheckboxList(
       index: 2,
@@ -39,8 +51,17 @@ class _OrderCardScreenState extends State<OrderCardScreen> {
     ),
   ];
 
+  DatabaseHelper dataBase = new DatabaseHelper();
+
+  @override
+  void initState() {
+    getInfo();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    blocCard.fetchAllCard();
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: PreferredSize(
@@ -211,7 +232,7 @@ class _OrderCardScreenState extends State<OrderCardScreen> {
               ),
             ),
             Container(
-              height: 200,
+              height: 150,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(10),
@@ -241,8 +262,150 @@ class _OrderCardScreenState extends State<OrderCardScreen> {
                       bottomRight: Radius.circular(10)),
                   color: AppTheme.white,
                 ),
-                child: Column(
-                  children: [],
+                child: StreamBuilder(
+                  stream: blocCard.allCard,
+                  builder: (context, AsyncSnapshot<List<ItemResult>> snapshot) {
+                    if (snapshot.hasData) {
+                      allCount = 0;
+                      allPrice = 0.0;
+                      for (int i = 0; i < snapshot.data.length; i++) {
+                        allCount += snapshot.data[i].cardCount;
+                        allPrice += (snapshot.data[i].cardCount * snapshot.data[i].price);
+                      }
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            child: Text(
+                              translate("orders.courier"),
+                              style: TextStyle(
+                                fontFamily: AppTheme.fontRoboto,
+                                fontWeight: FontWeight.w600,
+                                fontStyle: FontStyle.normal,
+                                fontSize: 17,
+                                color: AppTheme.black_text,
+                              ),
+                            ),
+                            margin: EdgeInsets.only(
+                                left: 16, right: 16, top: 16, bottom: 2),
+                          ),
+                          Container(
+                            child: Row(
+                              children: [
+                                Text(
+                                  allCount.toString()+" " + translate("item.tovar"),
+                                  style: TextStyle(
+                                    fontFamily: AppTheme.fontRoboto,
+                                    fontWeight: FontWeight.normal,
+                                    fontStyle: FontStyle.normal,
+                                    fontSize: 13,
+                                    color: AppTheme.black_transparent_text,
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Container(),
+                                ),
+                                GestureDetector(
+                                  child: Text(
+                                    translate("orders.edit_tovar"),
+                                    style: TextStyle(
+                                      fontFamily: AppTheme.fontRoboto,
+                                      fontWeight: FontWeight.normal,
+                                      fontStyle: FontStyle.normal,
+                                      fontSize: 13,
+                                      color: AppTheme.blue_app_color,
+                                    ),
+                                  ),
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                  },
+                                ),
+                              ],
+                            ),
+                            margin: EdgeInsets.only(
+                                left: 16, right: 16, bottom: 16),
+                          ),
+                          Expanded(
+                            child: ListView.builder(
+                              padding: EdgeInsets.only(left: 16,right: 16),
+                              scrollDirection: Axis.horizontal,
+                              itemCount: snapshot.data.length ,
+                              itemBuilder: (context, index) {
+                                return Container(
+                                  height: 66,
+                                  width: 66,
+                                  margin: EdgeInsets.only(bottom: 16),
+                                  child: Stack(
+                                    children: [
+                                      Align(
+                                        child: Container(
+                                          height: 56,
+                                          width: 56,
+                                          child: Center(
+                                            child: CachedNetworkImage(
+                                              height: 56,
+                                              width: 56,
+                                              imageUrl: snapshot.data[index ].image,
+                                              placeholder: (context, url) =>
+                                                  Icon(Icons.camera_alt),
+                                              errorWidget: (context, url, error) =>
+                                                  Icon(Icons.error),
+                                            ),
+                                          ),
+                                        ),
+                                        alignment: Alignment.bottomCenter,
+                                      ),
+                                      Align(
+                                        alignment: Alignment.topRight,
+                                        child: Container(
+                                          height: 24,
+                                          width: 24,
+                                          decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.circular(18.0),
+                                            color: Color(0xFF99A2AD),
+                                          ),
+                                          child: Center(
+                                            child: Text(
+                                              "x"+snapshot.data[index].cardCount.toString(),
+                                              style: TextStyle(
+                                                fontFamily: AppTheme.fontRoboto,
+                                                fontSize: 12,
+                                                color: AppTheme.white,
+                                                fontWeight: FontWeight.normal,
+                                                fontStyle: FontStyle.normal,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              },
+                            ),
+                          )
+                        ],
+                      );
+                    } else if (snapshot.hasError) {
+                      return Text(snapshot.error.toString());
+                    }
+                    return Shimmer.fromColors(
+                      baseColor: Colors.grey[300],
+                      highlightColor: Colors.grey[100],
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        shrinkWrap: true,
+                        physics: ClampingScrollPhysics(),
+                        itemCount: 10,
+                        itemBuilder: (_, __) => Container(
+                          height: 56,
+                          width: 56,
+                          color: AppTheme.white,
+                        ),
+                      ),
+                    );
+                  },
                 ),
               ),
             ),
@@ -287,7 +450,7 @@ class _OrderCardScreenState extends State<OrderCardScreen> {
             Container(
               margin: EdgeInsets.only(top: 26, left: 16, right: 16),
               child: Text(
-                "Shahboz Turonov",
+                fullName,
                 style: TextStyle(
                     fontFamily: AppTheme.fontRoboto,
                     fontWeight: FontWeight.normal,
@@ -311,7 +474,7 @@ class _OrderCardScreenState extends State<OrderCardScreen> {
             Container(
               margin: EdgeInsets.only(top: 21, left: 16, right: 16),
               child: Text(
-                "+998943293406",
+                number,
                 style: TextStyle(
                     fontFamily: AppTheme.fontRoboto,
                     fontWeight: FontWeight.normal,
@@ -489,7 +652,7 @@ class _OrderCardScreenState extends State<OrderCardScreen> {
                     child: Container(),
                   ),
                   Text(
-                    translate(5.toString()),
+                    allCount.toString(),
                     style: TextStyle(
                       fontSize: 13,
                       fontFamily: AppTheme.fontRoboto,
@@ -521,7 +684,8 @@ class _OrderCardScreenState extends State<OrderCardScreen> {
                     child: Container(),
                   ),
                   Text(
-                    translate("50 000" + translate("sum")),
+                    priceFormat.format(allPrice) +
+                        translate(translate("sum")),
                     style: TextStyle(
                       fontSize: 13,
                       fontFamily: AppTheme.fontRoboto,
@@ -553,7 +717,8 @@ class _OrderCardScreenState extends State<OrderCardScreen> {
                     child: Container(),
                   ),
                   Text(
-                    translate("50 000" + translate("sum")),
+                    priceFormat.format(allPrice) +
+                        translate(translate("sum")),
                     style: TextStyle(
                       fontSize: 15,
                       fontFamily: AppTheme.fontRoboto,
@@ -601,5 +766,25 @@ class _OrderCardScreenState extends State<OrderCardScreen> {
         ),
       ),
     );
+  }
+
+  Future<void> getInfo() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      var num = "+";
+      for (int i = 0; i < prefs.getString("number").length; i++) {
+        if (i == 3 || i == 5 || i == 8 || i == 10) {
+          num += " ";
+        }
+        num += prefs.getString("number")[i];
+      }
+      number = num;
+
+      var name = prefs.getString("name");
+      var surName = prefs.getString("surname");
+
+      fullName = name + " " + surName;
+    });
   }
 }
