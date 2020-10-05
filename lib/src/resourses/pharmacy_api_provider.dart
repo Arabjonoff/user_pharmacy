9ff5dd27835f609e886d86fd20e26958b89cb528
KOSSHOH <shahboz.turonov98@gmail.com>
2020-10-05 18:10:36 +0500

    error in payment

--- lib/src/resourses/pharmacy_api_provider.dart
+++ lib/src/resourses/pharmacy_api_provider.dart
@@ -685,19 +685,39 @@ class PharmacyApiProvider {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String token = prefs.getString("token");
 
-    HttpClient httpClient = new HttpClient();
-    httpClient
-      ..badCertificateCallback =
-          ((X509Certificate cert, String host, int port) => true);
-    HttpClientRequest request = await httpClient.postUrl(Uri.parse(url));
-    request.headers.set('content-type', 'application/json');
-    request.headers.set(HttpHeaders.authorizationHeader, "Bearer $token");
-    request.write(json.encode(verfy));
-    HttpClientResponse response = await request.close();
+    Map<String, String> headers = {
+      HttpHeaders.authorizationHeader: "Bearer $token",
+      'content-type': 'application/json; charset=utf-8',
+    };
+    try {
+      http.Response response = await http
+          .post(url, headers: headers, body: json.encode(verfy))
+          .timeout(const Duration(seconds: 10));
+      final Map responseJson = json.decode(utf8.decode(response.bodyBytes));
+      return PaymentVerfy.fromJson(responseJson);
+    } on TimeoutException catch (_) {
+      RxBus.post(BottomViewModel(1), tag: "EVENT_BOTTOM_VIEW_ERROR");
+      return PaymentVerfy(
+          error_code: -1, errorNote: translate("internet_error"));
+    } on SocketException catch (_) {
+      RxBus.post(BottomViewModel(1), tag: "EVENT_BOTTOM_VIEW_ERROR");
+      return PaymentVerfy(
+          error_code: -1, errorNote: translate("internet_error"));
+    }
 
-    String reply = await response.transform(utf8.decoder).join();
-    final Map parsed = json.decode(reply);
-    return PaymentVerfy.fromJson(parsed);
+    // HttpClient httpClient = new HttpClient();
+    // httpClient
+    //   ..badCertificateCallback =
+    //       ((X509Certificate cert, String host, int port) => true);
+    // HttpClientRequest request = await httpClient.postUrl(Uri.parse(url));
+    // request.headers.set('content-type', 'application/json');
+    // request.headers.set(HttpHeaders.authorizationHeader, "Bearer $token");
+    // request.write(json.encode(verfy));
+    // HttpClientResponse response = await request.close();
+    //
+    // String reply = await response.transform(utf8.decoder).join();
+    // final Map parsed = json.decode(reply);
+    // return PaymentVerfy.fromJson(parsed);
   }
 
   ///items
